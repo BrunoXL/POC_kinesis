d5350243648e4b8de1497fc46e8d3c66d65a1d89
bruno Xavier <bruno.xavier@calindra.com.br>
2021-08-26 12:43:49 -0300

    Fix name of producer files»

--- run_producer.py
+++ run_producer.py
@@ -1,6 +1,6 @@
 import subprocess
 
-if __name__ == "run_productor":
+if __name__ == "run_producter":
     def start():
         cmd = ["uvicorn", "poc_kinesis.productor:app", "--reload"]
         subprocess.run(cmd)
