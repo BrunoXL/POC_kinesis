f14c9b9ea6988946d75dc5e7d4dd76a7c8e0894b
bruno Xavier <bruno.xavier@calindra.com.br>
2021-10-03 13:16:09 -0300

    Chamada ao kinesis

--- poc_kinesis/service/producer_service.py
+++ poc_kinesis/service/producer_service.py
@@ -6,6 +6,7 @@ from fastapi import HTTPException
 async def producer_service():
     aws_secret: str
     aws_id: str
+    STREAM_NAME: 'poc_lambda_bruno'
 
     try:
         aws_secret = os.environ['AWS_SECRET_ACCESS_KEY']
@@ -13,4 +14,16 @@ async def producer_service():
     except Exception as e:
         logging.error(f"Variaveis de ambientes não definidas")
         raise HTTPException(status_code=401, detail=e.args)
-  
+
+    producer = boto3.client('kinesis', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret)
+
+    message: str = 'Teste: enviando menssagem ao kinesis'
+
+    producer.put_record(
+        StreamName=STREAM_NAME,
+        Data=message,
+        PartitionKey='partitionkey')
+
+
+def encode_messages(message):
+    return str.encode(message)
