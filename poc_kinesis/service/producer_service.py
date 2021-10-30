f2d539ef375d164a68ef52f6b0a9486044f0c427
bruno Xavier <bruno.xavier@calindra.com.br>
2021-10-29 23:14:08 -0300

    Adiciona log em producer_service

--- poc_kinesis/service/producer_service.py
+++ poc_kinesis/service/producer_service.py
@@ -3,8 +3,8 @@ import os
 import logging
 from fastapi import HTTPException
 
-log = logging.getLogger(__name__)
-log.setLevel("INFO")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def producer_service():
     aws_secret: str
@@ -15,21 +15,21 @@ async def producer_service():
         aws_secret = os.environ['AWS_SECRET_ACCESS_KEY']
         aws_id = os.environ['AWS_ACCESS_KEY_ID']
     except Exception as e:
-        log.error(f"Variaveis de ambientes não definidas")
+        logger.error(f"Variaveis de ambientes não definidas")
         raise HTTPException(status_code=401, detail=e.args)
 
     producer = boto3.client('kinesis', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret)
 
     message: str = 'Teste: enviando menssagem ao kinesis'
 
-    # log.info(f"Pronto para inserir mensagens em {STREAM_NAME}")
+    logger.info(f"Pronto para inserir mensagens em {STREAM_NAME}")
 
     producer.put_record(
         StreamName=STREAM_NAME,
         Data=message,
         PartitionKey='partitionkey')
 
-    # log.info(f"Mensagens inseridas com sucesso")
+    logger.info(f"Mensagens inseridas com sucesso")
 
 
 # def encode_messages(message):
